f4f328a653c89eda1a714c801d54436a49a652fe
zkactive <2446082839@qq.com>
2020-08-26 17:56:58 +0800

    创建订单模块。

---
+++ cloud-provider-payment8001/src/main/java/cn/zk/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package cn.zk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author  zk
+ * @date  2020年8月26日16:10:22
+ * @version 1.0
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
+
