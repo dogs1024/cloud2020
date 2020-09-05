bc8a1d650857af3c6e5aa6fa8381d5cf2c77ae41
zkactive <2446082839@qq.com>
2020-09-05 18:05:58 +0800

    1、解决springboot整合zookeeper问题。 2、解决SLF4J包冲突问题。 3、新建cloud-consumerzk-order80服务消费者模块（zookeeper）

--- cloud-provider-payment8004/src/main/java/cn/zk/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8004/src/main/java/cn/zk/springcloud/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package cn.zk.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,15 @@ import java.util.UUID;
  * @date 2020/9/1 17:45
  */
 @RestController
+@Slf4j
 public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
 
     @GetMapping(value = "/payment/zk")
-    public String paymentzk(){
-        return "springcloud with zookeeper port:"+serverPort+"\t"+ UUID.randomUUID().toString();
+    public String paymentzk() {
+        return "springcloud with zookeeper port:" + serverPort + "\t" + UUID.randomUUID().toString();
     }
 
 }
