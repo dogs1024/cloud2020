e34fc8b9c9fee984a0a5f05e1bc881c1b72ad8ed
zkactive <2446082839@qq.com>
2020-10-17 18:11:08 +0800

    1、SpringCloud Bus整合RabbitMQ。Config服务端动态刷新。——手动刷新。广播通知&定点通知。

---
+++ cloud-config-client-3366/src/main/java/cn/zk/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package cn.zk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zk
+ * @version 1.0
+ * @description Config客户端集群
+ * @date 2020/10/17 14:09
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/cn/zk/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,30 @@
+package cn.zk.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zk
+ * @version 1.0
+ * @description Config客户端集群。——测试动态刷新。
+ * @date 2020/10/17 14:30
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "serverPort:"+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+
+
+}
